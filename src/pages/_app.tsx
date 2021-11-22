9688db38403792a48ab76eb1a52285ac655b2092
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-11-22 11:58:56 -0500

    update the user details cache when user is created or logged in

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,61 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
 import { AppProps } from 'next/app';
-import { createClient, Provider } from 'urql';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
 
 import { NavBar } from '../components';
+import {
+  UserCreateMutation,
+  UserDetailsDocument,
+  UserDetailsQuery,
+  UserLoginMutation
+} from '../generated/graphql';
 import { default as theme } from '../theme';
 import './app.css';
 
+function updateQuery<ResultT, QueryT>(
+  cache: Cache,
+  queryInput: QueryInput,
+  result: any,
+  updateCallback: (result: ResultT, query: QueryT) => QueryT
+) {
+  return cache.updateQuery(queryInput, (query) => updateCallback(result, query as any) as any);
+}
+
 const client = createClient({
   url: 'http://localhost:4000/graphql',
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        userCreate: (result, args, cache, info) => {
+          // update userDetails when userCreate is called
+          updateQuery<UserCreateMutation, UserDetailsQuery>(
+            cache,
+            { query: UserDetailsDocument },
+            result,
+            (createResult, userDetailsData) => {
+              return {
+                userDetails: createResult.userCreate
+              };
+            }
+          );
+        },
+        userLogin: (result, args, cache, info) => {
+          // update userDetails when userLogin is called
+          updateQuery<UserLoginMutation, UserDetailsQuery>(
+            cache,
+            { query: UserDetailsDocument },
+            result,
+            (loginResult, userDetailsData) => {
+              return {
+                userDetails: loginResult.userLogin
+              };
+            }
+          );
+        }
+      }
+    }
+  }), fetchExchange],
   fetchOptions: {
     credentials: 'include'
   }
