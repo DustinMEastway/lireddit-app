6c00c0a858924422d2cbae2c14d19b52d6d4ca08
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-11-22 21:57:59 -0500

    add posts to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
+import { Text } from '@chakra-ui/layout';
 import { useRouter } from 'next/router';
 
 import { NavBar } from '../components';
 import { withUrqlClient } from '../core';
-import { useUserDetailsQuery } from '../generated/graphql';
+import { usePostsQuery, useUserDetailsQuery } from '../generated/graphql';
 
 export const Index: React.FC = () => {
-  const [{ data, fetching }] = useUserDetailsQuery();
+  const [ { data, fetching } ] = useUserDetailsQuery();
+  const [ { data: posts } ] = usePostsQuery();
   const router = useRouter();
 
   if (!fetching && !data?.userDetails) {
     router.push('/login');
   }
 
-  return (fetching) ? null : <>
+  return <>
     <NavBar />
-    Hello World!
+    {!posts ? <Text>Loading...</Text> : posts.posts.map((post) =>
+      <Text key={post.id}>{post.title}</Text>
+    )}
   </>;
 };
 
